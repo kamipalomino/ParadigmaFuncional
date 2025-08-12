5c389ecd881124a23736b08bdf9c6ec29faffcea
Cam Palomino <cpalomino@frba.utn.edu.ar>
2025-08-12 20:18:31 -0300

    Ejercicios

---
+++ Practica Funcional/ejercicios2.hs
@@ -0,0 +1,67 @@
+{-
+
+11. En una plantación de pinos, de cada árbol se conoce la altura expresada en cm. El peso de un pino se puede calcular a partir de la altura así: 
+Los pinos se usan para llevarlos a una fábrica de muebles, a la que le sirven árbolesvde entre 400 y 1000 kilos, un pino fuera de este rango no le sirve a la fábrica. Para
+
+esta situación:
+
+a. Definir la función pesoPino, recibe la altura de un pino y devuelve su peso.
+
+b. Definir la función esPesoUtil, recibe un peso en kg y devuelve True si un
+
+pino de ese peso le sirve a la fábrica, y False en caso contrario.
+
+c. Definir la función sirvePino, recibe la altura de un pino y devuelve True si
+
+un pino de ese peso le sirve a la fábrica, y False en caso contrario. Usar
+
+composición en la definición.
+
+12. Este ejercicio alguna vez se planteó como un Desafío Café con Leche: Implementar
+
+la función esCuadradoPerfecto/1, sin hacer operaciones con punto flotante.
+
+Ayuda: les va a venir bien una función auxiliar, tal vez de dos parámetros. Pensar
+
+que el primer cuadrado perfecto es 0, para llegar al 2do (1) sumo 1, para llegar al 3ro
+
+(4) sumo 3, para llegar al siguiente (9) sumo 5, después sumo 7, 9, 11 etc.. También
+
+algo de recursividad van a tener que usar
+-}
+
+type Medicion  = (Int, Int, Int)
+max3 :: Medicion -> Int
+max3 (a, b, c) = (max a)  (max b c)
+
+min3 :: Medicion -> Int
+min3 (a, b, c) = (min a)  (min b c)
+
+dispersion :: Medicion -> Int
+dispersion medida = max3 medida - min3 medida
+--Una dispersión se considera chica si es de menos de 30 cm
+-- grande si es de más de un metro.
+
+
+diasParejos :: Medicion -> Bool -- parejos si la dispersión es chica
+diasParejos = (< 30) . dispersion 
+
+diasLocos :: Medicion -> Bool
+diasLocos = (>100) . dispersion 
+
+diasNormales :: Medicion -> Bool 
+diasNormales medida = (not . diasParejos) medida && (not . diasLocos) medida
+
+--3 kg x cm hasta 3 metros, 2 kg x cm arriba de los 3 metros. P.ej. 2 metros ⇒ 600 kg, 5 metros ⇒ 1300 kg.
+
+pesoPino altura 
+  | altura < 300	= 3 * altura
+  | altura == 300	= 3* altura 
+  | otherwise 	= 2 * altura 
+
+main :: IO ()
+main = do
+--dia :: Medicion 
+--dia (4, 6
+let resultado = pesoPino 20 
+putStrLn ("el resultado es: "  ++  show resultado)
--- Practica Funcional/main.hs
+++ Practica Funcional/main.hs
@@ -1,10 +1,39 @@
+--Import GHC.Num
 -- Definimos una función que suma dos números
 suma :: Int -> Int -> Int
 suma x y = x + y
 
+--Definir la función esMultiploDe/2, que devuelve True si el segundo es múltiplo del primero,
+esMultiploDe :: Int -> Int -> Bool
+esMultiploDe num otroNum = mod otroNum num == 0
+
+esMultiploDeTres :: Int -> Bool
+esMultiploDeTres = esMultiploDe 3 
+
+cubo :: Int -> Int 
+cubo  =  (^3)
+
+--area :: Num -> Num -> Num
+area base altura = base + altura 
+
+esBisiesto n
+  | (not . esMultiploDe 100 ) n	&&	(esMultiploDe 4) n	= True 
+  | esMultiploDe 400 n	 = True 
+  | otherwise	 = False
+
+celsiusToFahr = (+32) . (*1.8)
+
+fahrToCelsius n =  ( n - 32) *0.55556
+
+haceFrioF = (<8) . fahrToCelsius 
 -- Función principal (main) que imprime el resultado
 main :: IO ()
 main = do
-    let resultado = suma 3 5
-    putStrLn ("La suma de 3 y 5 es: " ++ show resultado)
-    
+    let resultado = cubo 2
+    let areas = area 15.5 0.5
+    let bisiesto = esBisiesto 2000
+    let fahrenheit = celsiusToFahr 100
+    let celsius = fahrToCelsius 212
+    let frio = haceFrioF 32
+    putStrLn ("el resultado es: " ++ show frio)
+
